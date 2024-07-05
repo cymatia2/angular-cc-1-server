c90610284c237d193796cda8e2f154a18585c803
cymatia2 <jwjang@jangarchi.com>
2024-07-04 23:51:15 -0400

    home-e server-2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // Cors configuration - Allows requests from localhost:4200
 const corsOptions = {
